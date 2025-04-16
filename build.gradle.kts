803e343bee1446a3289d94e9b89f187a9396d0c7
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-04-16 11:12:21 +0200

    ⬆️ Småbumps, inkludert kotlin 2.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ private val flywayVersion = "11.5.0"
 private val ktorVersion = "3.1.2"
 
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
     id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
     id("com.gradleup.shadow") version "8.3.6"
 }
