a9de952728cb47d2f5ac8e674bda945f31dafcc6
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-28 13:01:12 +0200

    Mock versjoner av APIene vi bygger

--- src/main/kotlin/no/nav/helse/bakrommet/App.kt
+++ src/main/kotlin/no/nav/helse/bakrommet/App.kt
@@ -55,6 +55,25 @@ internal fun Application.appModul(dataSource: DataSource) {
         get("/antallBehandlinger") {
             call.respondText { dataSource.query("select count(*) from behandling")!! }
         }
+        post("/v1/personsok") {
+            call.response.headers.append("Content-Type", "application/json")
+            call.respondText("{ personId: '1234567891011' }")
+        }
+        get("/v1/{personId}/personinfo") {
+            call.response.headers.append("Content-Type", "application/json")
+            call.respondText(
+                """{
+        fødselsnummer: '62345678906',
+        aktørId: '1234567891011',
+        navn: 'Kalle Kranfører',
+        alder: 47,
+    }""",
+            )
+        }
+        post("/v1/{personId}/soknader") {
+            call.response.headers.append("Content-Type", "application/json")
+            call.respondText("[]")
+        }
     }
 }
 
