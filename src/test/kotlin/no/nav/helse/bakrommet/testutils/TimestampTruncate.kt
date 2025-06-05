809e85a538246049dc8949473f9ac9b8e5224d0e
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-06-05 16:26:40 +0200

    Mikrosekunder skulle det jo være, ikke nanosekunder
    Co-authored-by: Roy A G <43439497+royag-nav@users.noreply.github.com>

--- src/test/kotlin/no/nav/helse/bakrommet/testutils/TimestampTruncate.kt
+++ src/test/kotlin/no/nav/helse/bakrommet/testutils/TimestampTruncate.kt
@@ -2,11 +2,11 @@ package no.nav.helse.bakrommet.testutils
 
 import no.nav.helse.bakrommet.saksbehandlingsperiode.Dokument
 import no.nav.helse.bakrommet.saksbehandlingsperiode.Saksbehandlingsperiode
-import java.time.temporal.ChronoUnit.NANOS
+import java.time.temporal.ChronoUnit.MICROS
 import java.time.temporal.ChronoUnit.SECONDS
 
 fun Saksbehandlingsperiode.truncateTidspunkt() = copy(opprettet = opprettet.truncatedTo(SECONDS))
 
 fun Iterable<Dokument>.tidsstuttet() = map(Dokument::tidsstuttet)
 
-fun Dokument.tidsstuttet() = copy(opprettet = opprettet.truncatedTo(NANOS))
+fun Dokument.tidsstuttet() = copy(opprettet = opprettet.truncatedTo(MICROS))
