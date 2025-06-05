9467fa4a9b60da2db0ef31b853d2763b912e90eb
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-06-05 16:31:16 +0200

    Pga. avrunding må vi nok bruke millisekunder, ja
    Co-authored-by: Roy A G <43439497+royag-nav@users.noreply.github.com>

--- src/test/kotlin/no/nav/helse/bakrommet/testutils/TimestampTruncate.kt
+++ src/test/kotlin/no/nav/helse/bakrommet/testutils/TimestampTruncate.kt
@@ -2,11 +2,11 @@ package no.nav.helse.bakrommet.testutils
 
 import no.nav.helse.bakrommet.saksbehandlingsperiode.Dokument
 import no.nav.helse.bakrommet.saksbehandlingsperiode.Saksbehandlingsperiode
-import java.time.temporal.ChronoUnit.MICROS
+import java.time.temporal.ChronoUnit.MILLIS
 import java.time.temporal.ChronoUnit.SECONDS
 
 fun Saksbehandlingsperiode.truncateTidspunkt() = copy(opprettet = opprettet.truncatedTo(SECONDS))
 
 fun Iterable<Dokument>.tidsstuttet() = map(Dokument::tidsstuttet)
 
-fun Dokument.tidsstuttet() = copy(opprettet = opprettet.truncatedTo(MICROS))
+fun Dokument.tidsstuttet() = copy(opprettet = opprettet.truncatedTo(MILLIS))
