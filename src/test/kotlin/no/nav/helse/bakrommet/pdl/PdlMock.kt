e1746868e6bf0511763f86fb76119ad0c8bcb162
Roy A G <43439497+royag-nav@users.noreply.github.com>
2025-08-01 13:23:25 +0200

    gjør det mulig å angi ønsket pdl-reply for "mockPdl"

--- src/test/kotlin/no/nav/helse/bakrommet/pdl/PdlMock.kt
+++ src/test/kotlin/no/nav/helse/bakrommet/pdl/PdlMock.kt
@@ -8,36 +8,43 @@ import no.nav.helse.bakrommet.bodyToJson
 import no.nav.helse.bakrommet.mockHttpClient
 
 object PdlMock {
-    val mockPdl =
-        mockHttpClient { request ->
-            val auth = request.headers[HttpHeaders.Authorization]!!
-            if (auth != "Bearer ${TestOppsett.configuration.pdl.scope.oboTokenFor()}") {
+    val mockPdl = mockPdl()
 
-                respondError(HttpStatusCode.Unauthorized)
+    fun mockPdl(
+        identTilReplyMap: Map<String, String> =
+            mapOf(
+                "1234" to pdlReply(),
+                "01010199999" to pdlReply(),
+            ),
+    ) = mockHttpClient { request ->
+        val auth = request.headers[HttpHeaders.Authorization]!!
+        if (auth != "Bearer ${TestOppsett.configuration.pdl.scope.oboTokenFor()}") {
+            respondError(HttpStatusCode.Unauthorized)
+        } else {
+            val json = request.bodyToJson()
+            val ident = json["variables"]["ident"].asText()
+            val reply = identTilReplyMap[ident]
+            if (reply != null) {
+                respond(
+                    status = HttpStatusCode.OK,
+                    content = reply,
+                    headers = headersOf("Content-Type" to listOf("application/json")),
+                )
+            } else if (ident == "error") {
+                respond(
+                    status = HttpStatusCode.OK,
+                    content = pdlUgyldigIdentReply,
+                    headers = headersOf("Content-Type" to listOf("application/json")),
+                )
             } else {
-                val json = request.bodyToJson()
-                val ident = json["variables"]["ident"].asText()
-                if (ident == "1234" || ident == "01010199999") {
-                    respond(
-                        status = HttpStatusCode.OK,
-                        content = pdlReply,
-                        headers = headersOf("Content-Type" to listOf("application/json")),
-                    )
-                } else if (ident == "error") {
-                    respond(
-                        status = HttpStatusCode.OK,
-                        content = pdlUgyldigIdentReply,
-                        headers = headersOf("Content-Type" to listOf("application/json")),
-                    )
-                } else {
-                    respond(
-                        status = HttpStatusCode.OK,
-                        content = personIkkeFunnetReply,
-                        headers = headersOf("Content-Type" to listOf("application/json")),
-                    )
-                }
+                respond(
+                    status = HttpStatusCode.OK,
+                    content = personIkkeFunnetReply,
+                    headers = headersOf("Content-Type" to listOf("application/json")),
+                )
             }
         }
+    }
 
     val pdlClient =
         PdlClient(
@@ -46,18 +53,20 @@ object PdlMock {
             httpClient = mockPdl,
         )
 
-    private val pdlReply =
-        """
+    fun pdlReply(
+        fnr: String = "12345678910",
+        aktorId: String = "10987654321",
+    ) = """
         {
           "data": {
             "hentIdenter": {
               "identer": [
                 {
-                  "ident": "12345678910",
+                  "ident": "$fnr",
                   "gruppe": "FOLKEREGISTERIDENT"
                 },
                 {
-                  "ident": "10987654321",
+                  "ident": "$aktorId",
                   "gruppe": "AKTORID"
                 }
               ]
