a2b41a28c85bca6738936af614d4ec5c8b92bcdc
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-09-19 10:55:57 +0200

    💩 ktlint.........

--- src/test/kotlin/no/nav/helse/bakrommet/db/TestDataSource.kt
+++ src/test/kotlin/no/nav/helse/bakrommet/db/TestDataSource.kt
@@ -6,18 +6,24 @@ import no.nav.helse.bakrommet.infrastruktur.db.MedDataSource
 import org.testcontainers.containers.PostgreSQLContainer
 
 object TestDataSource {
-    val postgres = PostgreSQLContainer("postgres:17")
-        .withReuse(true)
-        .withLabel("app", "bakrommet")
-        .apply { start() }
+    val postgres =
+        PostgreSQLContainer("postgres:17")
+            .withReuse(true)
+            .withLabel("app", "bakrommet")
+            .apply { start() }
 
-    val configuration = Configuration.DB(
-        jdbcUrl = postgres.jdbcUrl + "&user=" + postgres.username + "&password=" + postgres.password
-    )
+    val configuration =
+        Configuration.DB(
+            jdbcUrl = postgres.jdbcUrl + "&user=" + postgres.username + "&password=" + postgres.password,
+        )
 
-    val dbModule = DBModule(configuration = Configuration.DB(
-        jdbcUrl = postgres.jdbcUrl + "&user=" + postgres.username + "&password=" + postgres.password
-    )).also { it.migrate() }
+    val dbModule =
+        DBModule(
+            configuration =
+                Configuration.DB(
+                    jdbcUrl = postgres.jdbcUrl + "&user=" + postgres.username + "&password=" + postgres.password,
+                ),
+        ).also { it.migrate() }
 
     fun resetDatasource() {
         val db = MedDataSource(dbModule.dataSource)
