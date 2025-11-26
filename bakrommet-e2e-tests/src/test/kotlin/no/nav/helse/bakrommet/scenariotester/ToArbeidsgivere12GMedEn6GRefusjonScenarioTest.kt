350c0fd184892fd60d09760ef382947f7f3dbb58
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-11-26 14:11:59 +0100

    Vi betaler ikke ut mer enn 6g ved 9g refusjon

--- bakrommet-e2e-tests/src/test/kotlin/no/nav/helse/bakrommet/scenariotester/ToArbeidsgivere12GMedEn6GRefusjonScenarioTest.kt
+++ bakrommet-e2e-tests/src/test/kotlin/no/nav/helse/bakrommet/scenariotester/ToArbeidsgivere12GMedEn6GRefusjonScenarioTest.kt
@@ -33,6 +33,32 @@ class ToArbeidsgivere12GMedEn6GRefusjonScenarioTest {
     }
 
     @Test
+    fun `to arbeidsgivere med 6g hvor den ene refunderer 9g`() {
+        val seksG = Grunnbeløp.`6G`.beløp(ScenarioDefaults.fom)
+
+        Scenario(
+            yrkesaktiviteter =
+                listOf(
+                    Arbeidstaker(
+                        "888",
+                        inntekt =
+                            Inntektsmelding(
+                                seksG.månedlig,
+                                Refusjonsperiode(ScenarioDefaults.fom, null, seksG.times(1.5).dtoMånedligDouble()),
+                            ),
+                        dagoversikt = SykAlleDager(),
+                    ),
+                    Arbeidstaker("999", inntekt = Inntektsmelding(seksG.månedlig), dagoversikt = SykAlleDager()),
+                ),
+        ).run {
+            `skal ha sykepengegrunnlag`(744168.0)
+            `skal ha refusjon`(28620, orgnummer = "888")
+            `skal ha refusjon`(0, orgnummer = "999")
+            `skal ha direkteutbetaling`(0)
+        }
+    }
+
+    @Test
     fun `to arbeidsgivere med 6g hvor den ene refunderer 6g den andre 3g`() {
         val seksG = Grunnbeløp.`6G`.beløp(ScenarioDefaults.fom)
 
