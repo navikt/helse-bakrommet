0d904da0a9aa52ae7b4c092296ed00e0fb082aed
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-01-12 09:34:04 +0100

    🐛 Håndter eksplisitt at person kanskje ikke finnes i pdl
    Co-authored-by: Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>

--- bakrommet-api/src/main/kotlin/no/nav/helse/bakrommet/api/person/PersonsøkRoute.kt
+++ bakrommet-api/src/main/kotlin/no/nav/helse/bakrommet/api/person/PersonsøkRoute.kt
@@ -1,10 +1,15 @@
 package no.nav.helse.bakrommet.api.person
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import no.nav.helse.bakrommet.api.auth.saksbehandlerOgToken
 import no.nav.helse.bakrommet.api.dto.person.PersonsøkRequestDto
+import no.nav.helse.bakrommet.api.errorhandling.respondProblem
 import no.nav.helse.bakrommet.api.serde.respondJson
 import no.nav.helse.bakrommet.errorhandling.InputValideringException
+import no.nav.helse.bakrommet.errorhandling.PersonIkkeFunnetException
+import no.nav.helse.bakrommet.errorhandling.ProblemDetails
 import no.nav.helse.bakrommet.person.NaturligIdent
 import no.nav.helse.bakrommet.person.PersonsøkService
 import java.lang.IllegalArgumentException
@@ -22,6 +27,21 @@ fun Route.personsøkRoute(
     post("/v1/personsok") {
         val request = call.receive<PersonsøkRequestDto>()
         val ident = request.naturligIdent()
+        runCatching {
+            service.hentIdenter(ident, call.saksbehandlerOgToken())
+        }.onFailure {
+            if (it is PersonIkkeFunnetException) {
+                val problemDetails =
+                    ProblemDetails(
+                        status = 404,
+                        title = "Person ikke funnet",
+                        detail = "Fant ikke person i PDL",
+                    )
+                call.respondProblem(HttpStatusCode.NotFound, problemDetails)
+            } else {
+                throw it
+            }
+        }
         val newPersonId = service.hentEllerOpprettPseudoId(ident)
         call.respondJson(newPersonId.tilPersonsøkResponseDto())
     }
