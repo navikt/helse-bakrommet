2d2b262593cb11dcfe70da2ad4946ed8a28f5e9c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2026-01-19 11:25:06 +0100

    ♻️ Skriver om slett yrkesaktivitet
    Co-authored-by: Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
    Co-authored-by: Roy A G <43439497+royag-nav@users.noreply.github.com>

--- bakrommet-services/src/main/kotlin/no/nav/helse/bakrommet/repository/YrkesaktivitetRepository.kt
+++ bakrommet-services/src/main/kotlin/no/nav/helse/bakrommet/repository/YrkesaktivitetRepository.kt
@@ -2,9 +2,14 @@ package no.nav.helse.bakrommet.repository
 
 import no.nav.helse.bakrommet.domain.saksbehandling.behandling.BehandlingId
 import no.nav.helse.bakrommet.domain.sykepenger.yrkesaktivitet.Yrkesaktivitet
+import no.nav.helse.bakrommet.domain.sykepenger.yrkesaktivitet.YrkesaktivitetId
 
 interface YrkesaktivitetRepository {
     fun finn(behandlingId: BehandlingId): List<Yrkesaktivitet>
 
+    fun finn(yrkesaktivitetId: YrkesaktivitetId): Yrkesaktivitet?
+
     fun lagre(yrkesaktivitet: Yrkesaktivitet)
+
+    fun slett(yrkesaktivitetId: YrkesaktivitetId)
 }
