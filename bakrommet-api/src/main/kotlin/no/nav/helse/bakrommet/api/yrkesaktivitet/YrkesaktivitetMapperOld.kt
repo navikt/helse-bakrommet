c2a45c1c182f2dcd3d3045f8101c4ce55d5f0cdb
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2026-01-19 13:02:15 +0100

    🔥 Fjerner yrkesaktivitet service
    Co-authored-by: Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
    Co-authored-by: Roy A G <43439497+royag-nav@users.noreply.github.com>

--- bakrommet-api/src/main/kotlin/no/nav/helse/bakrommet/api/yrkesaktivitet/YrkesaktivitetMapperOld.kt
+++ bakrommet-api/src/main/kotlin/no/nav/helse/bakrommet/api/yrkesaktivitet/YrkesaktivitetMapperOld.kt
@@ -1,66 +1,15 @@
 package no.nav.helse.bakrommet.api.yrkesaktivitet
 
-import no.nav.helse.bakrommet.api.dto.yrkesaktivitet.*
+import no.nav.helse.bakrommet.api.dto.yrkesaktivitet.KildeDto
+import no.nav.helse.bakrommet.api.dto.yrkesaktivitet.RefusjonsperiodeDto
 import no.nav.helse.bakrommet.behandling.dagoversikt.Kilde
-import no.nav.helse.bakrommet.behandling.yrkesaktivitet.Perioder
-import no.nav.helse.bakrommet.behandling.yrkesaktivitet.Periodetype
 import no.nav.helse.bakrommet.behandling.yrkesaktivitet.Refusjonsperiode
-import no.nav.helse.bakrommet.behandling.yrkesaktivitet.YrkesaktivitetMedOrgnavn
-import no.nav.helse.bakrommet.behandling.yrkesaktivitet.domene.Dagoversikt
-import no.nav.helse.dto.PeriodeDto as SpleisPeriodeDto
-
-fun YrkesaktivitetMedOrgnavn.tilYrkesaktivitetDto(): YrkesaktivitetDto =
-    YrkesaktivitetDto(
-        id = yrkesaktivitet.id,
-        kategorisering = yrkesaktivitet.kategorisering.tilYrkesaktivitetKategoriseringDto(),
-        dagoversikt = yrkesaktivitet.dagoversikt?.tilMergetDagoversikt(),
-        generertFraDokumenter = yrkesaktivitet.generertFraDokumenter.map { it },
-        perioder = yrkesaktivitet.perioder?.tilPerioderDto(),
-        inntektRequest = yrkesaktivitet.inntektRequest?.tilInntektRequestDto(),
-        inntektData = yrkesaktivitet.inntektData?.tilInntektDataDto(),
-        refusjon = yrkesaktivitet.refusjon?.map { it.tilRefusjonsperiodeDto() },
-        orgnavn = orgnavn,
-    )
-
-fun Dagoversikt.tilMergetDagoversikt(): List<DagDto> {
-    val avslagsdagerMap = this.avslagsdager.associateBy { it.dato }
-    return this.sykdomstidlinje.map { dag -> dag.tilDagDto() }.map { dag ->
-        val avslagsdag = avslagsdagerMap[dag.dato]
-        if (avslagsdag != null) {
-            dag.copy(
-                dagtype = DagtypeDto.Avslått,
-                avslåttBegrunnelse = avslagsdag.avslåttBegrunnelse,
-            )
-        } else {
-            dag
-        }
-    }
-}
 
 fun Kilde.tilKildeDto(): KildeDto =
     when (this) {
         Kilde.Søknad -> KildeDto.Søknad
         Kilde.Saksbehandler -> KildeDto.Saksbehandler
     }
-
-fun Perioder.tilPerioderDto(): PerioderDto =
-    PerioderDto(
-        type = type.tilPeriodetypeDto(),
-        perioder = perioder.map { it.tilPeriodeDto() },
-    )
-
-fun Periodetype.tilPeriodetypeDto(): PeriodetypeDto =
-    when (this) {
-        Periodetype.ARBEIDSGIVERPERIODE -> PeriodetypeDto.ARBEIDSGIVERPERIODE
-        Periodetype.VENTETID -> PeriodetypeDto.VENTETID
-        Periodetype.VENTETID_INAKTIV -> PeriodetypeDto.VENTETID_INAKTIV
-    }
-
-fun SpleisPeriodeDto.tilPeriodeDto(): PeriodeDto =
-    PeriodeDto(
-        fom = fom,
-        tom = tom,
-    )
 
 fun Refusjonsperiode.tilRefusjonsperiodeDto(): RefusjonsperiodeDto =
     RefusjonsperiodeDto(
