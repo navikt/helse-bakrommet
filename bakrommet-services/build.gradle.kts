b83bd56ec00e1f1df09f2808725060b7eccebfda
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2026-01-19 19:05:07 +0100

    🐛 Fikser i dependencies etter at common forsvant

--- bakrommet-services/build.gradle.kts
+++ bakrommet-services/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     api(project(":bakrommet-domain"))
     api(project(":bakrommet-kafka-dto"))
     api(project(":sykepenger-model"))
-
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     // TODO flytte ut til client igjen når vi har en egen domene-representasjon av IM
     api("no.nav.sykepenger.kontrakter:inntektsmelding-kontrakt:2025.04.04-01-56-365d3")
 
@@ -19,9 +19,16 @@ dependencies {
 
     implementation("com.github.navikt.spleis:sykepenger-okonomi")
 
-    implementation("ch.qos.logback:logback-classic")
+    implementation("com.github.navikt.spleis:sykepenger-okonomi")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("net.logstash.logback:logstash-logback-encoder")
-    implementation("org.slf4j:slf4j-api")
+    api("ch.qos.logback:logback-classic")
+    api("org.slf4j:slf4j-api")
+
+    api("com.fasterxml.jackson.core:jackson-core")
+    api("com.fasterxml.jackson.core:jackson-databind")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
     testImplementation(testFixtures(project(":bakrommet-services")))
 
